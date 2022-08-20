dd37c2ab6d85b29755cf3ca4bdd41dd1a9c8dc49
ASUS <adrian.veas2016@uteq.edu.ec>
2022-08-20 14:25:26 -0500

    MEJORA MENU DINAMICO

--- app/src/main/java/com/example/navigationview/MainActivity.kt
+++ app/src/main/java/com/example/navigationview/MainActivity.kt
@@ -6,6 +6,9 @@ import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.navigationview.modelos.users
 import com.example.navigationview.modelos.users.Companion.JsonObjectsBuild
 import org.json.JSONArray
@@ -15,10 +18,13 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     //var toolbar: Toolbar? = null
     lateinit var user: EditText
     lateinit var password: EditText
-
-    val listauser: String = "[{\"nickname\":\"USER1\", \"password\":\"12345\", \"urlimage\":\"ic_user1_foreground\", \"typeuser\":\"Admin\" }," +
+    private lateinit var url :  String
+    lateinit var lstUserArray : JSONArray
+    lateinit var lstUsuarios: ArrayList<users>
+    /*val listauser: String = "[{\"nickname\":\"USER1\", \"password\":\"12345\", \"urlimage\":\"ic_user1_foreground\", \"typeuser\":\"Admin\" }," +
             "{\"nickname\":\"USER2\", \"password\":\"12345\", \"urlimage\":\"ic_user2_foreground\" , \"typeuser\":\"Student\"}," +
             "{\"nickname\":\"USER3\", \"password\":\"12345\", \"urlimage\":\"ic_user3_foreground\", \"typeuser\":\"Profesor\"}]"
+    */
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,30 +50,41 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         try{
             user = findViewById(R.id.editusername)
             password = findViewById(R.id.editpassword)
-
-            val lstUserArray = JSONArray(listauser)
-            val lstUsuarios: ArrayList<users> = JsonObjectsBuild(lstUserArray)
             var confirmation = "N"
-            for (users in lstUsuarios){
-                if(users.nickname == user.text.toString()){
-                    if (users.password == password.text.toString()){
-                        confirmation = "Y"
+            url = "https://mocki.io/v1/1a099137-7ee7-422d-a56f-cb37533e6974"
+
+            val queue = Volley.newRequestQueue(this)
+            val stringRequest = StringRequest(
+                Request.Method.GET, url,
+                { response ->
+                    lstUserArray = JSONArray(response)
+                    lstUsuarios = JsonObjectsBuild(lstUserArray)
+                    for (users in lstUsuarios){
+                        if(users.nickname == user.text.toString()){
+                            if (users.password == password.text.toString()){
+                                confirmation = "Y"
+                            }
+                        }
                     }
-                }
-            }
+                    val intent = Intent(this, MainActivity2::class.java)
+                    val bundle = Bundle()
 
-            val intent = Intent(this, MainActivity2::class.java)
-            val bundle = Bundle()
+                    if(confirmation == "Y") {
+                        bundle.putString("user", user.text.toString())
+                        bundle.putString("listusers", listauser)
 
-            if(confirmation == "Y") {
-                bundle.putString("user", user.text.toString())
-                bundle.putString("listusers", listauser)
+                        intent.putExtras(bundle)
+                        startActivity(intent)
+                    }else{
+                        Toast.makeText(applicationContext, "Error User or Password", Toast.LENGTH_SHORT).show()
+                    }
+                },
+                { error ->
+                    Toast.makeText(applicationContext, error.message, Toast.LENGTH_SHORT).show()})
 
-                intent.putExtras(bundle)
-                startActivity(intent)
-            }else{
-                Toast.makeText(applicationContext, "Error User or Password", Toast.LENGTH_SHORT).show()
-            }
+
+            queue.add(stringRequest)
+
         }
         catch (ex: Exception){
             Toast.makeText(applicationContext, "Error: "+ex.message, Toast.LENGTH_SHORT).show()
